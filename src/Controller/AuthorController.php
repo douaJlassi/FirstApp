ac9f38672f452157449d04c2e107648daf4c88a6
douaJlassi <Doua.Jlassi@esprit.tn>
2025-10-18 15:46:04 +0100

    atelier3_Exercice2

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -16,11 +16,42 @@ final class AuthorController extends AbstractController
         ]);
     }
 
-     #[Route('/showAuthor/{name}', name: 'app_showAuthor')]
+    #[Route('/showAuthor/{name}', name: 'app_showAuthor')]
     public function showAuthor($name): Response
     {
         return $this->render('author/show.html.twig', [
             'author_name' => $name,
+        ]);
+    }
+
+    #[Route('/listAuthor', name: 'app_listAuthors')]
+    public function listAuthor(): Response
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/VictorHugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william.jpg', 'username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/TahaHessin.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+        ]);
+    }
+    #[Route('/detailsAuthor/{id}', name: 'app_detailsAuthors')]
+    public function detailsAuthor($id){
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/VictorHugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william.jpg', 'username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/TahaHessin.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
+
+        $author=null;
+        foreach ($authors as $a) {
+            if ($a['id'] == $id) {
+                $author = $a;
+            }
+        }
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
         ]);
     }
 }
